d7341f5bee23f200c80750e46dcdf2b139a75cd9
KeenanSegenchuk <71797879+KeenanSegenchuk@users.noreply.github.com>
2024-07-19 11:50:53 -0400

    Added github.js
    Added library for interacting with github repo thru API calls
    Origin/Docs: https://github.com/github-tools/github
    Guide: https://www.audero.it/blog/2015/10/17/upload-files-on-github-using-github-js/

---
+++ github-js/test/issue.spec.js
@@ -0,0 +1,277 @@
+import expect from 'must';
+
+import Github from '../lib/GitHub';
+import testUser from './fixtures/user.js';
+import wait from './helpers/wait';
+import {assertSuccessful} from './helpers/callbacks';
+import getTestRepoName from './helpers/getTestRepoName';
+
+describe('Issue', function() {
+   let github;
+   const testRepoName = getTestRepoName();
+   let remoteIssues;
+
+   before(function() {
+      github = new Github({
+         username: testUser.USERNAME,
+         password: testUser.PASSWORD,
+         auth: 'basic',
+      });
+
+      return github
+         .getUser()
+         .createRepo({name: testRepoName})
+         .then(wait(5000))
+         .then(function() {
+            remoteIssues = github.getIssues(testUser.USERNAME, testRepoName);
+            return remoteIssues.createIssue({
+               title: 'Test issue',
+               body: 'Test issue body',
+            });
+         })
+         .then(function() {
+            return remoteIssues.createMilestone({
+               title: 'Default Milestone',
+               description: 'Test',
+            });
+         });
+   });
+
+   after(function() {
+      return github.getRepo(testUser.USERNAME, testRepoName).deleteRepo();
+   });
+
+   describe('reading', function() {
+      let remoteIssueId;
+      let milestoneId;
+
+      it('should list issues', function(done) {
+         remoteIssues.listIssues({}, assertSuccessful(done, function(err, issues) {
+            expect(issues).to.be.an.array();
+            remoteIssueId = issues[0].number;
+
+            done();
+         }));
+      });
+
+      it('should get issue', function(done) {
+         remoteIssues.getIssue(remoteIssueId, assertSuccessful(done, function(err, issue) {
+            expect(issue).to.have.own('number', remoteIssueId);
+
+            done();
+         }));
+      });
+
+      it('should get issue events', function() {
+         return remoteIssues.listIssueEvents(remoteIssueId)
+            .then(function({data: events}) {
+               expect(events).to.be.an.array();
+            });
+      });
+
+      it('should get all milestones', function(done) {
+         remoteIssues.listMilestones()
+            .then(({data: milestones}) => {
+               expect(milestones).to.be.an.array();
+               milestoneId = milestones[0].number;
+
+               done();
+            }).catch(done);
+      });
+
+      it('should get a single milestone', function(done) {
+         remoteIssues.getMilestone(milestoneId)
+            .then(({data: milestone}) => {
+               expect(milestone).to.have.own('title', 'Default Milestone');
+               done();
+            }).catch(done);
+      });
+   });
+
+   describe('creating/editing/deleting', function() {
+      let createdIssueId;
+      let issueCommentId;
+      let createdMilestoneId;
+      let createdLabel;
+
+      // 200ms between tests so that Github has a chance to settle
+      beforeEach(function(done) {
+         setTimeout(done, 200);
+      });
+
+      it('should create issue', function(done) {
+         const newIssue = {
+            title: 'New issue',
+            body: 'New issue body',
+         };
+
+         remoteIssues.createIssue(newIssue, assertSuccessful(done, function(err, issue) {
+            createdIssueId = issue.number;
+            expect(issue).to.have.own('url');
+            expect(issue).to.have.own('title', newIssue.title);
+            expect(issue).to.have.own('body', newIssue.body);
+
+            done();
+         }));
+      });
+
+      it('should edit issue', function(done) {
+         const newProps = {
+            title: 'Edited title',
+            state: 'closed',
+         };
+
+         remoteIssues.editIssue(createdIssueId, newProps, assertSuccessful(done, function(err, issue) {
+            expect(issue).to.have.own('title', newProps.title);
+
+            done();
+         }));
+      });
+
+      it('should post issue comment', function(done) {
+         remoteIssues.createIssueComment(createdIssueId, 'Comment test', assertSuccessful(done, function(err, issue) {
+            expect(issue).to.have.own('body', 'Comment test');
+
+            done();
+         }));
+      });
+
+      it('should list issue comments', function(done) {
+         remoteIssues.listIssueComments(createdIssueId, assertSuccessful(done, function(err, comments) {
+            expect(comments).to.be.an.array();
+            expect(comments[0]).to.have.own('body', 'Comment test');
+            issueCommentId = comments[0].id;
+            done();
+         }));
+      });
+
+      it('should get a single issue comment', function(done) {
+         remoteIssues.getIssueComment(issueCommentId, assertSuccessful(done, function(err, comment) {
+            expect(comment).to.have.own('body', 'Comment test');
+            done();
+         }));
+      });
+
+      it('should edit issue comment', function(done) {
+         remoteIssues.editIssueComment(issueCommentId, 'Comment test edited',
+            assertSuccessful(done, function(err, comment) {
+               expect(comment).to.have.own('body', 'Comment test edited');
+
+               done();
+            }));
+      });
+
+      it('should delete issue comment', function(done) {
+         remoteIssues.deleteIssueComment(issueCommentId, assertSuccessful(done, function(err, response) {
+            expect(response).to.be.true();
+
+            done();
+         }));
+      });
+
+      it('should create a milestone', function(done) {
+         let milestone = {
+            title: 'v42',
+            description: 'The ultimate version',
+         };
+
+         remoteIssues.createMilestone(milestone)
+            .then(({data: createdMilestone}) => {
+               expect(createdMilestone).to.have.own('number');
+               expect(createdMilestone).to.have.own('title', milestone.title);
+
+               createdMilestoneId = createdMilestone.number;
+               done();
+            }).catch(done);
+      });
+      it('should update a milestone', function(done) {
+         let milestone = {
+            description: 'Version 6 * 7',
+         };
+
+         expect(createdMilestoneId).to.be.a.number();
+         remoteIssues.editMilestone(createdMilestoneId, milestone)
+            .then(({data: createdMilestone}) => {
+               expect(createdMilestone).to.have.own('number', createdMilestoneId);
+               expect(createdMilestone).to.have.own('description', milestone.description);
+
+               done();
+            }).catch(done);
+      });
+      it('should delete a milestone', function(done) {
+         expect(createdMilestoneId).to.be.a.number();
+         remoteIssues.deleteMilestone(createdMilestoneId)
+            .then(({status}) => {
+               expect(status).to.equal(204);
+               done();
+            }).catch(done);
+      });
+
+      it('should create a label', (done) => {
+         let label = {
+            name: 'test',
+            color: '123456',
+         };
+
+         remoteIssues.createLabel(label)
+            .then(({data: _createdLabel}) => {
+               expect(_createdLabel).to.have.own('name', label.name);
+               expect(_createdLabel).to.have.own('color', label.color);
+
+               createdLabel = label.name;
+               done();
+            }).catch(done);
+      });
+
+      it('should retrieve a single label', (done) => {
+         let label = {
+            name: 'test',
+            color: '123456',
+         };
+
+         remoteIssues.getLabel(label.name)
+            .then(({data: retrievedLabel}) => {
+               expect(retrievedLabel).to.have.own('name', label.name);
+               expect(retrievedLabel).to.have.own('color', label.color);
+
+               done();
+            }).catch(done);
+      });
+
+      it('should update a label', (done) => {
+         let label = {
+            color: '789abc',
+            name: createdLabel,
+         };
+
+         expect(createdLabel).to.be.a.string();
+         remoteIssues.editLabel(createdLabel, label)
+            .then(({data: updatedLabel}) => {
+               expect(updatedLabel).to.have.own('name', createdLabel);
+               expect(updatedLabel).to.have.own('color', label.color);
+
+               done();
+            }).catch(done);
+      });
+
+      it('should list labels', (done) => {
+         expect(createdLabel).to.be.a.string();
+
+         remoteIssues.listLabels({}, assertSuccessful(done, function(err, labels) {
+            expect(labels).to.be.an.array();
+            const hasLabel = labels.some((label) => label.name === createdLabel);
+            expect(hasLabel).to.be.true();
+            done();
+         }));
+      });
+
+      it('should delete a label', (done) => {
+         expect(createdLabel).to.be.a.string();
+         remoteIssues.deleteLabel(createdLabel)
+            .then(({status}) => {
+               expect(status).to.equal(204);
+               done();
+            }).catch(done);
+      });
+   });
+});
---
+++ github-js/test/team.spec.js
@@ -0,0 +1,189 @@
+import expect from 'must';
+
+import Github from '../lib/GitHub';
+import testUser from './fixtures/user.js';
+import {assertFailure} from './helpers/callbacks';
+import getTestRepoName from './helpers/getTestRepoName';
+
+const altUser = {
+   USERNAME: 'mtscout6-test',
+};
+
+function createTestTeam() {
+   const name = getTestRepoName();
+
+   const github = new Github({
+      username: testUser.USERNAME,
+      password: testUser.PASSWORD,
+      auth: 'basic',
+   });
+
+   const org = github.getOrganization(testUser.ORGANIZATION);
+
+   return org.createTeam({
+      name,
+      privacy: 'closed',
+   }).then(({data: result}) => {
+      const team = github.getTeam(result.id);
+      return {team, name};
+   });
+}
+
+let team;
+let name;
+
+describe('Team', function() { // Isolate tests that are based on a fixed team
+   before(function() {
+      const github = new Github({
+         username: testUser.USERNAME,
+         password: testUser.PASSWORD,
+         auth: 'basic',
+      });
+
+      const org = github.getOrganization(testUser.ORGANIZATION);
+
+      /* eslint-disable no-console */
+      // The code below add a fixed-test-repo-1
+      let promiseRepo = new Promise((resolve) => {
+         org
+            .createRepo({name: 'fixed-test-repo-1'})
+            .then(resolve, () => {
+               console.log('skiped fixed-test-repo-1 creation');
+               resolve();
+            });
+      });
+
+      // The code below add a fixed-test-repo-1
+      let promiseTeam = org
+            .createTeam({
+               name: 'fixed-test-repo-1',
+               repo_names: [testUser.ORGANIZATION + '/fixed-test-repo-1'], // eslint-disable-line camelcase
+            })
+            .then(({data: team}) => team)
+            .catch(() => {
+               console.log('skiped fixed-test-repo-1 creation');
+               // Team already exists, fetch the team
+               return org.getTeams().then(({data: teams}) => {
+                  let team = teams
+                     .filter((team) => team.name === 'fixed-test-repo-1')
+                     .pop();
+                  if (!team) {
+                     throw new Error('missing fixed-test-repo-1');
+                  }
+                  return team;
+               });
+            });
+      /* eslint-enable no-console */
+
+      return promiseRepo.then(() => {
+         return promiseTeam
+         .then((t) => {
+            team = github.getTeam(t.id);
+            return team;
+         })
+         .then((team) => {
+            let setupTeam = [
+               team.addMembership(altUser.USERNAME),
+               team.addMembership(testUser.USERNAME),
+               team.manageRepo(testUser.ORGANIZATION, 'fixed-test-repo-1'),
+            ];
+            return Promise.all(setupTeam);
+         });
+      });
+   });
+
+   it('should get membership for a given user', function() {
+      return team.getMembership(altUser.USERNAME)
+         .then(function({data}) {
+            expect(data.state).to.equal('active');
+            expect(data.role).to.equal('member');
+         });
+   });
+
+   it('should list the users in the team', function() {
+      return team.listMembers()
+         .then(function({data: members}) {
+            expect(members).to.be.an.array();
+
+            const hasTestUser = members.some((member) => member.login === testUser.USERNAME);
+
+            expect(hasTestUser).to.be.true();
+         });
+   });
+
+   it('should get team repos', function() {
+      return team.listRepos()
+         .then(({data}) => {
+            const hasRepo = data.some((repo) => repo.name === 'fixed-test-repo-1');
+
+            expect(hasRepo).to.be.true();
+         });
+   });
+
+   it('should get team', function() {
+      return team.getTeam()
+         .then(({data}) => {
+            expect(data.name).to.equal('fixed-test-repo-1');
+         });
+   });
+
+   it('should check if team manages repo', function() {
+      return team.isManagedRepo(testUser.ORGANIZATION, 'fixed-test-repo-1')
+         .then((result) => {
+            expect(result).to.be.true();
+         });
+   });
+});
+
+describe('Team', function() { // Isolate tests that need a new team per test
+   beforeEach(function() {
+      return createTestTeam()
+         .then((x) => {
+            team = x.team;
+            name = x.name;
+         });
+   });
+
+   // Test for Team deletion
+   afterEach(function(done) {
+      team.deleteTeam()
+         .then(() => team.getTeam(assertFailure(done)));
+   });
+
+   it('should update team', function() {
+      const newName = `${name}-updated`;
+      return team.editTeam({name: newName})
+         .then(function({data}) {
+            expect(data.name).to.equal(newName);
+         });
+   });
+
+   it('should add membership for a given user', function() {
+      return team.addMembership(altUser.USERNAME)
+         .then(({data}) => {
+            const {state, role} = data;
+            expect(state === 'active' || state === 'pending').to.be.true();
+            expect(role).to.equal('member');
+         });
+   });
+
+   it('should add membership as a maintainer for a given user', function() {
+      return team.addMembership(altUser.USERNAME, {role: 'maintainer'})
+         .then(({data}) => {
+            const {state, role} = data;
+            expect(state === 'active' || state === 'pending').to.be.true();
+            expect(role).to.equal('maintainer');
+         });
+   });
+
+   it('should add/remove team management of repo', function() {
+      return team.manageRepo(testUser.ORGANIZATION, 'fixed-test-repo-1', {permission: 'pull'})
+         .then((result) => {
+            expect(result).to.be.true();
+            return team.unmanageRepo(testUser.ORGANIZATION, 'fixed-test-repo-1');
+         })
+         .then((result) => {
+            expect(result).to.be.true();
+         });
+   });
+});
