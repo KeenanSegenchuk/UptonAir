43bd3db9f88e00a7f937dc6ee16c4b5fb8ec370e
syedshazli <syed.abdu1818@gmail.com>
2024-07-25 19:13:51 -0400

    created the server

--- flask-server/server.py
+++ flask-server/server.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 # Members API Route
 @app.route("/members")
 def members():
-    return {"members":["Syed", "Keenan", "Peter"]}
+    return {"members":["Syed", "Keenan", "Peter, Laurie"]}
 
 if __name__ == "__main__":
     app.run(debug=True)
