87ac05113b76f500cdd1143bb014cbd2f20684e3
syedshazli <syed.abdu1818@gmail.com>
2024-07-24 21:59:13 -0400

    Config for front and back end

--- flask-server/server.py
+++ flask-server/server.py
@@ -1,1 +1,12 @@
 # This is our backend file
+from flask import Flask
+
+app = Flask(__name__)
+
+# Members API Route
+@app.route("/members")
+def members():
+    return {"members":["Member1", "Member2", "Member3"]}
+
+if __name__ == "__main__":
+    app.run(debug=True)
