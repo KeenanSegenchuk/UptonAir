c265a682bf353f1db378799c37b0ee49fd130305
syedshazli <syed.abdu1818@gmail.com>
2024-07-24 22:32:34 -0400

    error occurs in loading frontend. Loading console, members arent received from backend

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState, useEffect} from 'react'
 
 function App() {
+  const [data,setData] = useState([{}])
+
+  //fetches the API
+  useEffect(()=>{
+    fetch("/members").then(
+      res=>res.json()
+    ).then(
+      data=>{
+        setData(data)
+        //see if we retrieved from backend
+
+        console.log(data)
+      }
+    )
+  },[])
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <div>
+      
+      </div>
+  )
 }
 
-export default App;
+export default App
