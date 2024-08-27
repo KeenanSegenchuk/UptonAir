f4c9863f1306c695cbcd729665a7a7cb5d0c5158
KeenanSegenchuk <71797879+KeenanSegenchuk@users.noreply.github.com>
2024-08-27 18:26:12 -0400

    update flask server

---
+++ flask-server/get.py
@@ -0,0 +1,71 @@
+from time import time
+
+def get(window):
+
+	def find(T, data):
+		t = int(data[len(data)-1].split(",")[0])
+		T = t - T
+		
+		top = len(data) - 1
+		bottom = 0
+		while T != t:
+			t = int(data[int((top+bottom)/2)].split(",")[0])
+			#print(f'sample time: {t}, looking for {T}.')
+			if T > t:
+				bottom = int((top+bottom)/2 + 1)
+			elif T < t:
+				top = int((top+bottom)/2 - 1)
+			if abs(top - bottom) <= 1:
+				return bottom;
+		return int((top+bottom)/2)
+
+	t = ""
+	num = 0
+	times = ["month", "year", "day", "week", "minute", "hour", "YTD"]
+	
+	num = [int(s) for s in window.split() if s.isdigit()]
+	num = num[0];
+
+	for T in times:
+		if T in window:
+			t = T
+			break
+	
+	file = open("data.txt")
+	data = file.read().splitlines()[1:]
+
+	if t == "month": 
+		num = num * 30 * 24 * 60 * 60
+	elif t == "year":
+		num = num * 365 * 24 * 60 * 60
+	elif t == "week":
+		num = num * 60 * 60 * 24 * 7
+	elif t == "day":
+		num = num * 60 * 60 * 24
+	elif t == "hour":
+		num = num * 60 * 60
+	elif t == "minute":
+		num = num * 60
+	elif t == "YTD":
+		ctime = int(data[len(data)-1].split(",")[0])
+		num = int(time.strftime("%j", time.localtime(ctime))) * 24 * 60 * 60
+
+	data = data[find(num, data):]
+	newdata = []
+	for row in data:
+		newrow = []
+		for num in row.split(","):
+			newrow = newrow + [float(num)]
+		newdata += [newrow]
+	
+	return newdata
+
+def avg(data):
+	col = 1
+	sum = 0
+	count = 0
+	for line in data:
+		val = line.split(",")[col]
+		sum = sum + col
+		count = count + 1
+	return sum/count
--- flask-server/server.py
+++ flask-server/server.py
@@ -1,16 +1,68 @@
 # This is our backend file
-from flask import Flask
-
+import matplotlib
+matplotlib.use('Agg')
+from flask import Flask, render_template, request
+from clean import *
+from get import *
+import matplotlib.pyplot as plt 
 app = Flask(__name__)
+
+qrl = "quality.html"
+frl = "Z:/Projects/UptonAir/flask-server/"
 
 # Members API Route
 @app.route("/members")
 def members():
-    return {"members":["Syed", "Keenan", "Peter", "Laurie"]}
+	return {"members":["Syed", "Keenan", "Peter", "Laurie"]}
 
-@app.route("/quality")
+@app.route("/quality", methods =['POST', 'GET'])
 def quality():
-    return{"7/30/2024":["Bad until 7:30 PM", "Good after 7:30 PM"], "7/31/2024":["Ok until 5:30 PM", "Poor after 5:30 PM"]}
+	return render_template(qrl)
+
+@app.route("/")
+def home():
+	return render_template(qrl)
+
+@app.route("/get_plot", methods =['POST', 'GET'])
+def get_plot():
+	if request.method == "POST":
+		timeframes = request.form['window'].split(",")
+		plots = []
+
+		count = 0
+		for timeframe in timeframes:
+			data = get(timeframe)
+			x = [row[0] for row in data]
+			y = [row[3] for row in data]
+			#print(f'PLOTTING DATA: \n X: {x} \n Y: {y}')
+			plt.plot(x, y)
+			plt.title("Air quality over past " + timeframe)
+			plt.xlabel(timeframe)
+			plt.ylabel("PM_2.5 Readings")
+
+			plt.savefig("figs/aq_plot" + str(count) + ".png")
+			plots.append("figs/aq_plot" + str(count) + ".png")
+			count = count + 1
+		print(plots)
+		html = render_template(qrl, plot_url = plots, frl = frl)
+		print(html)
+		return render_template(qrl, plot_url = plots, frl = frl)
+	else:
+		return render_template(qrl)
+
+@app.route("/p", methods =['POST', 'GET'])
+def p():
+	with open("pull.py") as pull:
+		exec(pull.read())
+		clean()
+	return render_template(qrl, updated = "True")
+@app.route("/c", methods =['POST', 'GET'])
+def c():
+	clean()
+	return render_template(qrl, updated = "True")
+	
+
+    
 
 if __name__ == "__main__":
-    app.run(debug=True)
+	app.run(debug=True)
