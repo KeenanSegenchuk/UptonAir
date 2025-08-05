4da2b5c7d00d53085c7b0ce72ffc87cf142b1279
KeenanSegenchuk <71797879+KeenanSegenchuk@users.noreply.github.com>
2025-08-05 14:56:34 -0400

    fixed routing and style import on alerts page

--- client/src/pages/Alerts.js
+++ client/src/pages/Alerts.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import axios from 'axios';
-//import "../App.css";
+import "../App.css";
 import "../DeepseekCSSorcery/Alerts.css";
 import LinkButton from "../components/LinkButton";
 import { useAppContext } from "../AppContext";
