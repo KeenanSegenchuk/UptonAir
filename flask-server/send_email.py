c011ed7676f9f503c0b26ad486ac303ba4ff95bf
KeenanSegenchuk <71797879+KeenanSegenchuk@users.noreply.github.com>
2025-08-14 14:15:31 -0400

    Update send_email.py

--- flask-server/send_email.py
+++ flask-server/send_email.py
@@ -138,18 +138,19 @@ def send_email2(alert_obj):
           <strong>Overall Average AQI:</strong> {AQI} (over the last {avg_window} minutes)
         </p>
 
-        {triggered_sensors_html ? <h3 style="margin-top: 24px;">🚨 Sensors That Triggered The Alert</h3>
-        <table style="border-collapse: collapse; margin-top: 10px;">
-          <thead>
-            <tr style="background-color: #f2f2f2;">
-              <th style="text-align: left; padding: 6px 12px;">Sensor</th>
-              <th style="text-align: left; padding: 6px 12px;">Avg AQI</th>
-            </tr>
-          </thead>
-          <tbody>
-            {triggered_sensors_html}
-          </tbody>
-        </table> : ""}
+        f"""{'<h3 style="margin-top: 24px;">🚨 Sensors That Triggered The Alert</h3>'
+        '<table style="border-collapse: collapse; margin-top: 10px;">'
+          '<thead>'
+            '<tr style="background-color: #f2f2f2;">'
+              '<th style="text-align: left; padding: 6px 12px;">Sensor</th>'
+              '<th style="text-align: left; padding: 6px 12px;">Avg AQI</th>'
+            '</tr>'
+          '</thead>'
+          '<tbody>'
+            f'{triggered_sensors_html}'
+          '</tbody>'
+        '</table>' if triggered_sensors_html else ""}"""
+
         <h3 style="margin-top: 24px;">🟢 Other Sensors Monitored By This Alert </h3>
         <table style="border-collapse: collapse; margin-top: 10px;">
           <thead>
