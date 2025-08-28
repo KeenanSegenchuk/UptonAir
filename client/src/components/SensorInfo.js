e537bc847b2abc8a3cb08893a543698bb82ec373
KeenanSegenchuk <71797879+KeenanSegenchuk@users.noreply.github.com>
2025-08-28 14:29:03 -0400

    Update SensorInfo.js

--- client/src/components/SensorInfo.js
+++ client/src/components/SensorInfo.js
@@ -120,7 +120,7 @@ function SensorInfo({ dummy }) {
     return (
 	<div id="SensorInfo.js" className="sensorDiv">
 	    <div className={mobileMultiSelect ? "hideMobile" : ""}>		    
-	        <h1 style={{fontSize:isMobile ? "1.5em" : "3em", padding: "10px"}} className="headerText">Current Sensor: {getObj(`%{sensor_id}`)} [{getObj(`W${units}`)}]</h1>
+	        <h1 style={{fontSize:isMobile ? "1.5em" : "3em", padding: "10px"}} className="headerText">Current Sensor: {getObj(`${sensor_id}`)} [{getObj(`W${units}`)}]</h1>
 	        <Banner avg={Math.round(100*filteredData().banner_avg) / 100} units = {units}/>
 
     	        <h1 className="headerText">Recent Averages ({getObj(`U${units}`)})</h1>
