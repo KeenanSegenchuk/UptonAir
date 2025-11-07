2da91b1ad33019e7abbfbad886dc1b909850cac2
KeenanSegenchuk <71797879+KeenanSegenchuk@users.noreply.github.com>
2025-11-07 07:55:30 -0500

    Update EGraph.js

--- client/src/components/EGraph.js
+++ client/src/components/EGraph.js
@@ -199,7 +199,7 @@ function EGraph() {
 	if(bars.x) {
   	  bars = {
             type: "bar",
-            name: (typeof b.sensor === "string") ? b.sensor : getObj("$" + b.sensor),
+            name: (typeof l.sensor === "string" && !/\d/.test(l.sensor)) ? b.sensor : getObj("$" + b.sensor),
             data: bars.x.map((timestamp, i) => ({
                 value: [timestamp, bars.y[i]],
                 itemStyle: { color: getObj(`X${bars.y[i]}${units}`) }
