0ad24efbca7317b5d5274a66f8a8bd6dc58858a6
KeenanSegenchuk <71797879+KeenanSegenchuk@users.noreply.github.com>
2026-03-01 19:18:40 -0500

    Update pgUtil.py

--- flask-server/pgUtil.py
+++ flask-server/pgUtil.py
@@ -91,17 +91,17 @@ def formatLines(lines, format = "tuple"):
 	#check for invalid values
 	print("Formatting pulled lines")
 	lines = [
-		line[0:3] + [
+		line[0:2] + [float(-1)] if line[2] == "null" else [line[2]] + [
 			int(float(line[3]) < float(line[4])),
 			min(float(line[3]), float(line[4])),
 			999, 999, 999
 		] if float(line[3]) > 750 and float(line[4]) > 750 else
-		line[0:3] + [
+		line[0:2] + [float(-1)] if line[2] == "null" else [line[2]] + [
 			int(float(line[3]) < float(line[4])),
 			min(float(line[3]), float(line[4])),
 			-1, -1, -1
 		] if float(line[3]) > 750 or float(line[4]) > 750 else
-		line + PMtoAQI(line[2], line[3], line[4])
+		line[0:2] + [float(-1)] if line[2] == "null" else [line[2]] + line[3:] + PMtoAQI(line[2], line[3], line[4])
 		for line in lines
 	]
 	print("done formatting!")
